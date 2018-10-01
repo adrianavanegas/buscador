f68b16ff9849b106d094c97a06a4e30e7e05eb73
adrianavanegas <adrivp4@gmail.com>
2018-10-01 14:16:03 -0500

    Delete buscador.php

--- buscador.php
+++
@@ -1,8 +0,0 @@
-<?php
-  require('./registro.php');
-  $filtroCiudad=$_GET['filtro']['Ciudad'];
-  $filtroTipo=$_GET['filtro']['tipo'];
-  $filtroPrecio=$_GET['filtro']['precio'];
-  $obtenerDatos=leerDatos();
-  filtrarDatos($filtroCiudad,$filtroTipo,$filtroPrecio,$obtenerDatos)
- ?>
